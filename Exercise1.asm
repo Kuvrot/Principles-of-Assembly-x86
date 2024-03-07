a564944fc1b38e399b41dbe81a5e731f553f0214
Jaime Arturo <jamessanz@outlook.com>
2024-03-07 13:41:51 -0600

    Add files via upload

---
+++ Exercise1.asm
@@ -0,0 +1,17 @@
+section .data
+    msg db "Hello world!" , 0xa
+    len equ $- msg
+    
+section .text
+    global _start:
+    
+_start:
+    mov eax , 0x4
+    mov ebx , 0x1
+    mov ecx , msg
+    mov edx , len
+    int 0x80
+    
+    mov eax , 0x1
+    xor ebx , ebx
+    int 0x80
